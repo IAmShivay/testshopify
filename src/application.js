a266a4f14f29fed7f2d60efda86171b68593fb08
IAmShivay <shivaysharma77894@gmail.com>
2025-06-27 16:32:21 +0530

    Add Alpine.js script for enhanced interactivity

---
+++ src/application.js
@@ -0,0 +1,9 @@
+// Import Alpine.js
+import Alpine from 'alpinejs'
+
+// Start Alpine.js
+window.Alpine = Alpine
+Alpine.start()
+
+// Put your application javascript here
+console.log('Alpine.js loaded and started!')
